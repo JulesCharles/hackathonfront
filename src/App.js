daadda39143335c5c4363aa33bc6fec44b233a41
ohdreey <vallieaudrey@gmail.com>
2022-03-17 20:16:02 +0100

    recuperation données mariem

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import Fonda from './screens/Fonda'
-import Reponse from './components/Reponse'
+import Fonda from './screens/Fonda';
+import Reponse from './components/Reponse';
 // import { Routes, Route } from 'react-router-dom'
 
-import NavBar from './components/Navbar'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/Navbar';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
+      <Admin />
       <Routes>
         <Route path='/Fonda' element={<Fonda />}></Route>
         <Route path='/Reponse' element={<Reponse />}></Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
